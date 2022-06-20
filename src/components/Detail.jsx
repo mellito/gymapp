15c1afeab30ea6b44c272f5785cb09c851a5e20f
mellito <dayanalexismanrique@hotmail.com>
2022-06-19 19:39:11 -0500

    finish exercise details

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,8 +1,68 @@
 import React from "react";
+import { Typography, Stack, Button } from "@mui/material";
 
+import BodyPartImage from "../assets/icons/body-part.png";
+import TargetImage from "../assets/icons/target.png";
+import EquipmentImage from "../assets/icons/equipment.png";
 const Detail = ({ exerciseDetail }) => {
-  console.log(exerciseDetail);
-  return <div>Detail</div>;
+  const { name, gifUrl, bodyPart, target, equipment } = exerciseDetail;
+  const extraDetail = [
+    {
+      icon: BodyPartImage,
+      name: bodyPart,
+    },
+    {
+      icon: TargetImage,
+      name: target,
+    },
+    {
+      icon: EquipmentImage,
+      name: equipment,
+    },
+  ];
+
+  return (
+    <Stack
+      gap="60px"
+      sx={{ flexDirection: { lg: "row" }, p: "20px", alignItems: "center" }}
+    >
+      <img src={gifUrl} alt={name} className="detail-image" />
+      <Stack sx={{ gap: { lg: "35px", xs: "20px" } }}>
+        <Typography variant="h3">{name}</Typography>
+        <Typography variant="h6">
+          Exercises keep you strong. {name} but is one of the best exercises to
+          target your {target}. It will help you improve your mood and gain
+          enery
+        </Typography>
+        {extraDetail.map((detail) => (
+          <Stack
+            direction="row"
+            gap="10px"
+            key={detail.name}
+            alignItems="center"
+          >
+            <Button
+              sx={{
+                background: "#fff2bd",
+                borderRadius: "50%",
+                width: "100px",
+                height: "100px",
+              }}
+            >
+              <img
+                src={detail.icon}
+                alt={detail.name}
+                style={{ width: "50px", height: "50px" }}
+              />
+            </Button>
+            <Typography variant="h5" textTransform="capitalize">
+              {detail.name}
+            </Typography>
+          </Stack>
+        ))}
+      </Stack>
+    </Stack>
+  );
 };
 
 export default Detail;
